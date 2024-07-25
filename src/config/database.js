3a93d5d886706cec06a4639a0e5adc62c1fc6df4
Ni-mie <owoeyeebun46@gmail.com>
2024-07-25 12:46:01 +0100

    variable adjusted

--- src/config/database.js
+++ src/config/database.js
@@ -2,9 +2,9 @@ const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize(process.env.DB_NAME,
    process.env.DB_USER, process.env.DB_PASSWORD, {
-  host: '127.0.0.1',
-  port: 3307,
-  dialect: 'mysql',
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 3306,
+    dialect: 'mysql',
 });
 
 module.exports = sequelize;
