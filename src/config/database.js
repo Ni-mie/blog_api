a09b10f74b4c29fac529307dea22acc24fabf9c6
Ni-mie <owoeyeebun46@gmail.com>
2024-07-25 13:19:38 +0100

    last commit

--- src/config/database.js
+++ src/config/database.js
@@ -2,8 +2,8 @@ const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize(process.env.DB_NAME,
    process.env.DB_USER, process.env.DB_PASSWORD, {
-    host: process.env.DB_HOST || db,
-    port: process.env.DB_PORT || 3307,
+    host: process.env.DB_HOST || 'db',
+    port: process.env.DB_PORT || 3306,
     dialect: 'mysql',
 });
 
