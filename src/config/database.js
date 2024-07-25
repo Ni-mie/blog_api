6dbe11b5534c66bd33b3c1ccc37757c9328ac432
Ni-mie <owoeyeebun46@gmail.com>
2024-07-25 14:00:46 +0100

    correct docker

--- src/config/database.js
+++ src/config/database.js
@@ -2,8 +2,7 @@ const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize(process.env.DB_NAME,
    process.env.DB_USER, process.env.DB_PASSWORD, {
-    host: process.env.DB_HOST || 'db',
-    port: process.env.DB_PORT || 3306,
+    host: process.env.DB_HOST,
     dialect: 'mysql',
 });
 
